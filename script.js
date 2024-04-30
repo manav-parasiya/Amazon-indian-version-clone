d036fd155f1349915cf9ebc3792fe4a8d6cddd18
manav parasiya <manavparasiya1020@gmail.com>
2024-04-30 10:38:39 +0530

    added slider

--- script.js
+++ script.js
@@ -23,4 +23,134 @@ document.addEventListener("DOMContentLoaded", function() {
     prevBtn.addEventListener("click", prevSlide);
     nextBtn.addEventListener("click", nextSlide);
   });
+
+
+  let parent = document.querySelector(".fake-product-card");
   
+  // img.setAttribute("class","fake-product-card-img");
+  
+  
+  
+
+  
+ 
+
+let fun = async () =>
+{
+  await fetch("https://fakestoreapi.com/products")
+  .then ((res,req) =>
+  {
+    console.log("fetch response : - " , res);
+    return res.json();
+  })
+  .then ((res,req) =>
+  {
+    console.log("fetch response 2 : - " , res);
+    for (const singleProduct of res) {
+      // console.log(singleProduct.image);
+      let card = document.createElement("div");
+      card.style.height = "200px";
+      card.style.width = "200px";
+      card.style.backgroundColor = "blue";
+      let h4 = document.createElement("h4");
+      h4.innerText = singleProduct.title;
+      card.appendChild(h4);
+      let img = document.createElement("img");
+      img.setAttribute("src", singleProduct.image);
+      img.setAttribute("alt", "no image");
+      // console.log("singleProduct.image : - " ,singleProduct.image);
+      // console.log(singleProduct.image);
+      card.appendChild(img);
+      let h5 = document.createElement("h5");
+      card.appendChild(h5);
+      h5.innerText = singleProduct.price;
+      parent.appendChild(card);
+      console.log("single product id  : - " , singleProduct.id);
+      console.log("single product title  : - " , singleProduct.title);
+      console.log("single product price  : - " , singleProduct.price);
+      console.log("single product category  : - " , singleProduct.category);
+      console.log("single product description  : - " , singleProduct.description);
+      console.log("single product image  : - " , singleProduct.image);
+      console.log("single product rating  : - " , singleProduct.rating);
+      
+      
+    }
+    return res;
+  })
+  .catch((err) =>
+  {
+    console.log("Get an Error : - " , err);
+  }) 
+}
+
+fun()
+
+
+const initSlider = () => {
+  const imageList = document.querySelector(".slider-wrapper .image-list");
+  const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
+  const sliderScrollbar = document.querySelector(".container-5 .slider-scrollbar");
+  const scrollbarThumb = sliderScrollbar.querySelector(".scrollbar-thumb");
+  const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
+  
+  // Handle scrollbar thumb drag
+  scrollbarThumb.addEventListener("mousedown", (e) => {
+      const startX = e.clientX;
+      const thumbPosition = scrollbarThumb.offsetLeft;
+      const maxThumbPosition = sliderScrollbar.getBoundingClientRect().width - scrollbarThumb.offsetWidth;
+      
+      // Update thumb position on mouse move
+      const handleMouseMove = (e) => {
+          const deltaX = e.clientX - startX;
+          const newThumbPosition = thumbPosition + deltaX;
+
+          // Ensure the scrollbar thumb stays within bounds
+          const boundedPosition = Math.max(0, Math.min(maxThumbPosition, newThumbPosition));
+          const scrollPosition = (boundedPosition / maxThumbPosition) * maxScrollLeft;
+          
+          scrollbarThumb.style.left = `${boundedPosition}px`;
+          imageList.scrollLeft = scrollPosition;
+      }
+
+      // Remove event listeners on mouse up
+      const handleMouseUp = () => {
+          document.removeEventListener("mousemove", handleMouseMove);
+          document.removeEventListener("mouseup", handleMouseUp);
+      }
+
+      // Add event listeners for drag interaction
+      document.addEventListener("mousemove", handleMouseMove);
+      document.addEventListener("mouseup", handleMouseUp);
+  });
+
+  // Slide images according to the slide button clicks
+  slideButtons.forEach(button => {
+      button.addEventListener("click", () => {
+          const direction = button.id === "prev-slide" ? -1 : 1;
+          const scrollAmount = imageList.clientWidth * direction;
+          imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+      });
+  });
+
+   // Show or hide slide buttons based on scroll position
+  const handleSlideButtons = () => {
+      slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "flex";
+      slideButtons[1].style.display = imageList.scrollLeft >= maxScrollLeft ? "none" : "flex";
+  }
+
+  // Update scrollbar thumb position based on image scroll
+  const updateScrollThumbPosition = () => {
+      const scrollPosition = imageList.scrollLeft;
+      const thumbPosition = (scrollPosition / maxScrollLeft) * (sliderScrollbar.clientWidth - scrollbarThumb.offsetWidth);
+      scrollbarThumb.style.left = `${thumbPosition}px`;
+  }
+
+  // Call these two functions when image list scrolls
+  imageList.addEventListener("scroll", () => {
+      updateScrollThumbPosition();
+      handleSlideButtons();
+  });
+}
+
+window.addEventListener("resize", initSlider);
+window.addEventListener("load", initSlider);
